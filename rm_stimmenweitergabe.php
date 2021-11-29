7ad9949c2b0031485ff8e086742c2a49fec0276e
Simon Demuth <sidemuth705@googlemail.com>
2021-11-29 18:44:24 +0100

    Bugfix: Logikfehler

--- rm_stimmenweitergabe.php
+++ rm_stimmenweitergabe.php
@@ -8,16 +8,24 @@ if (!isset($_SESSION["email"])) {
 
 require("php/sec/mysql.php");
 
-$stmt = $mysql->prepare("SELECT * FROM users_voted WHERE von = '" . $_SESSION["email"] . "'");
+$stmt = $mysql->prepare("SELECT * FROM `stimmen_weitergaben` WHERE `stimmen_weitergaben`.`VON` = '" . $_SESSION["email"] . "';");
 $stmt->execute();
 $count = $stmt->rowCount();
 if ($count == 1) {
-    echo "<script>alert('Fehler: Der User, an den du deine Stimme weitergegeben hast, hat bereits abgestimmt. Du kannst deine Stimme nun nicht mehr zurückfordern!.')</script>";
-} else {
+    $row = $stmt->fetch();
+    $zu_email = $row["ZU"];
 
-    $stmt = $mysql->prepare("DELETE FROM `stimmen_weitergaben` WHERE `stimmen_weitergaben`.`VON` = '" . $_SESSION["email"] . "';");
-
+    $stmt = $mysql->prepare("SELECT * FROM users_voted WHERE VON = '" . $zu_email . "'");
     $stmt->execute();
+    $count = $stmt->rowCount();
+    if ($count == 1) {
+        echo "<script>alert('Fehler: Der User, an den du deine Stimme weitergegeben hast, hat bereits abgestimmt. Du kannst deine Stimme nun nicht mehr zurückfordern!.')</script>";
+    } else {
+
+        $stmt = $mysql->prepare("DELETE FROM `stimmen_weitergaben` WHERE `stimmen_weitergaben`.`VON` = '" . $_SESSION["email"] . "';");
+
+        $stmt->execute();
+    }
 }
 
 header('Location:stimme_weitergeben.php');
