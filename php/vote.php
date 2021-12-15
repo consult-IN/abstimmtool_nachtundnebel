0b9ee531f11260c09eef0d5d16488d233f1a07a9
di-math <sidemuth705@googlemail.com>
2021-12-15 10:35:45 +0100

    Stimmen-Splitting - done, funktioniert aber nicht..

--- php/vote.php
+++ php/vote.php
@@ -23,41 +23,67 @@ function vote($selected, $email, $wahl_id){
             $selected_votes = 0;
 
             for($i = 0; $i<count($selected); $i = $i + 2){
-                $selected_votes = $selected_votes + $selected[$i];
+                $selected_votes = $selected_votes + intval($selected[$i+1]);
             }
 
             if($stimmen > $selected_votes){
                 print("<script>alert('Error: Du wolltest mehr Stimmen abgeben, als du hast!')</script>");
                 $result = 3;
             }else{
-                //Enthaltungen?..
-                $stmt = $mysql->prepare("SELECT * FROM stimmen_weitergaben WHERE VON = :email"); 
-                $stmt->bindParam(":email", $email);
-                $stmt->execute();
-                $stmt->execute();
-                $count = $stmt->rowCount();
-                if ($count == 1) {
-                    $stimmen = -1;
-                    print("<script>alert('Achtung! Du hast deine Stimme an eine andere Person weitergegeben! Deine Stimme zählt nicht.')</script>");
-                }
+                
+                if($selected_votes<$stimmen){
 
-                if($stimmen < 0){
-                    print("<script>alert('Error: Negative Stimmenzahl')</script>");
-                    $result = 2;
+                    $selected = "EXEEROR";
+                    $insert_vote = 0;
+                    $diff = ($stimmen - $selected_votes);
+
+                    for($i = 1; $i<=$diff; $i++){
+                        $stmt = $mysql->prepare("INSERT INTO voting_results (WAHL_ID, ITEM, VOTES) VALUES (:wahl_id, :selected, :stimmen)"); 
+                        $stmt->bindParam(":selected", $selected);
+                        $stmt->bindParam(":wahl_id", $wahl_id);
+                        $stmt->bindParam(":stimmen", $insert_vote);
+                        $stmt->execute();
+                    }
+
+                    $stmt = $mysql->prepare("INSERT INTO users_voted (VON, WAHL_ID) VALUES (:email, :wahl_id)");
+                    $stmt->bindParam(":email", $email);
+                    $stmt->bindParam(":wahl_id", $wahl_id);
+                    $stmt->execute();
+                    $result = 0;
+
                 }else{
-                //iterate through array
-                $stmt = $mysql->prepare("INSERT INTO voting_results (WAHL_ID, ITEM, VOTES) VALUES (:wahl_id, :selected, :stimmen)"); 
-                $stmt->bindParam(":selected", $selected);
-                $stmt->bindParam(":wahl_id", $wahl_id);
-                $stmt->bindParam(":stimmen", $stimmen);
-                $stmt->execute();
 
-                $stmt = $mysql->prepare("INSERT INTO users_voted (VON, WAHL_ID) VALUES (:email, :wahl_id)");
-                $stmt->bindParam(":email", $email);
-                $stmt->bindParam(":wahl_id", $wahl_id);
-                $stmt->execute();
-                $result = 0;
-                }   
+                    $stmt = $mysql->prepare("SELECT * FROM stimmen_weitergaben WHERE VON = :email"); 
+                    $stmt->bindParam(":email", $email);
+                    $stmt->execute();
+                    $stmt->execute();
+                    $count = $stmt->rowCount();
+                    if ($count == 1) {
+                        $stimmen = -1;
+                        print("<script>alert('Achtung! Du hast deine Stimme an eine andere Person weitergegeben! Deine Stimme zählt nicht.')</script>");
+                    }
+
+                    if($stimmen < 0){
+                        print("<script>alert('Error: Negative Stimmenzahl')</script>");
+                        $result = 2;
+                    }else{
+                    //iterate through array.. 0er auslassen
+                    for($i = 0; $i<=count($selected); $i = $i + 2){
+                        $stmt = $mysql->prepare("INSERT INTO voting_results (WAHL_ID, ITEM, VOTES) VALUES (:wahl_id, :selected, :stimmen)"); 
+                        $stmt->bindParam(":selected", $selected[$i]);
+                        $stmt->bindParam(":wahl_id", $wahl_id);
+                        $votes_to_insert = $stimmen[$i+2];
+                        $stmt->bindParam(":stimmen", $votes_to_insert);
+                        $stmt->execute();
+                    }
+
+                    $stmt = $mysql->prepare("INSERT INTO users_voted (VON, WAHL_ID) VALUES (:email, :wahl_id)");
+                    $stmt->bindParam(":email", $email);
+                    $stmt->bindParam(":wahl_id", $wahl_id);
+                    $stmt->execute();
+                    $result = 0;
+                    }
+                }
             
             }
 
